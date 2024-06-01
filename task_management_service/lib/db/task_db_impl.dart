7dce14092adb5022a2e253241a6cb129c84178f4
Difta Fitrahul Qihaj <difta.fit2002@mail.ugm.ac.id>
2024-06-01 23:09:19 +0700

    feat: add task db

---
+++ task_management_service/lib/db/task_db_impl.dart
@@ -0,0 +1,35 @@
+import 'package:task_management_service/db/task_DB_abstrac.dart';
+import 'package:task_management_service/models/task.dart';
+
+class TaskDatabaseClass implements TaskDatabaseAbstract{
+  @override
+  Future<void> createTask(Task task) {
+    // TODO: implement createTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteTask(int taskId) {
+    // TODO: implement deleteTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Task>> getAllTask() {
+    // TODO: implement getAllTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Task> getTask(int taskId) {
+    // TODO: implement getTask
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateTask(Task task) {
+    // TODO: implement updateTask
+    throw UnimplementedError();
+  }
+  
+}
