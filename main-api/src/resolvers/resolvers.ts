ae23a8853ee67b960951b0413faf05001d347914
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-10 13:38:06 +0700

    feat: remove placeholder for project and task management

--- main-api/src/resolvers/resolvers.ts
+++ main-api/src/resolvers/resolvers.ts
@@ -1,24 +1,10 @@
-import ProjectManagementAPI from '../dataSources/ProjectManagementAPI';
-import TaskManagementAPI from '../dataSources/TaskManagementAPI';
 import NotificationAPI from '../dataSources/NotificationAPI';
 
-const projectManagementAPI = new ProjectManagementAPI();
-const taskManagementAPI = new TaskManagementAPI();
+// const projectManagementAPI = new ProjectManagementAPI();
+// const taskManagementAPI = new TaskManagementAPI();
 const notificationAPI = new NotificationAPI();
 
 const root = {
-  getProjects: async () => {
-    return projectManagementAPI.getProjects();
-  },
-  getTasks: async ({ projectId }: { projectId: string }) => {
-    return taskManagementAPI.getTasks(projectId);
-  },
-  createProject: async ({ name, description }: { name: string; description: string }) => {
-    return projectManagementAPI.createProject({ name, description });
-  },
-  createTask: async ({ projectId, name, description }: { projectId: string; name: string; description: string }) => {
-    return taskManagementAPI.createTask(projectId, { name, description });
-  },
   sendNotification: async ({ message }: { message: string }) => {
     return notificationAPI.produceMessage({ message });
   },
