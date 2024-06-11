70f9bf9c57cb96fbac5898e8c0c23ee3fad450fd
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-11 19:40:42 +0700

    feat: map tm-service/delete-task to main-api

--- main-api/src/routes/taskManagementRoutes.ts
+++ main-api/src/routes/taskManagementRoutes.ts
@@ -7,5 +7,6 @@ taskManagementRoutes.get("/:taskId/:userId", taskManagementServices.getOneTask);
 taskManagementRoutes.post("/", taskManagementServices.createTask);
 taskManagementRoutes.put("/:taskId", taskManagementServices.editTask);
 taskManagementRoutes.get("/user/:userId", taskManagementServices.getAllTask);
+taskManagementRoutes.delete("/:taskId/:userId", taskManagementServices.deleteTask);
 
 export default taskManagementRoutes;
--- main-api/src/services/task-management-service.ts
+++ main-api/src/services/task-management-service.ts
@@ -5,12 +5,10 @@ const taskManagementURL = process.env.TASK_MANAGEMENT_SERVICE;
 
 const getOneTask = (req: Request, res: Response) => {
   const { taskId, userId } = req.params;
-  console.log(taskManagementURL + `/tasks/${taskId}?userId=${userId}`);
   axios
     .get(taskManagementURL + `/tasks/${taskId}?userId=${userId}`)
     .then((resp) => {
       res.send(resp.data);
-      console.log("response:", resp)
     })
     .catch((err) => {
       console.log(err);
@@ -53,7 +51,23 @@ const editTask = (req: Request, res: Response) => {
     });
 };
 
-const deleteTask = (req: Request, res: Response) => {};
+const deleteTask = (req: Request, res: Response) => {
+  const { taskId, userId } = req.params;
+  console.log(taskManagementURL + `/tasks/${taskId}?userId=${userId}`);
+  axios
+    .delete(taskManagementURL + `/tasks/${taskId}?userId=${userId}`)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (isAxiosError(err)) {
+        console.log(err.response?.data);
+        return res.status(Number(err.response?.status)).send({ error: err.name, detail: err.response?.data });
+      }
+      res.status(500).send(err);
+    });
+};
 
 const getAllTask = (req: Request, res: Response) => {
   const { userId } = req.params;
