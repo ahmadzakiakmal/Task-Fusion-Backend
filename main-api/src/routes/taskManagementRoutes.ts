a06e0b0ab7b8981ea45912a299868394c663412a
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-12 06:38:25 +0700

    feat: auto send message to Kafka on task creation

--- main-api/src/routes/taskManagementRoutes.ts
+++ main-api/src/routes/taskManagementRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
 import { taskManagementServices } from "../services/task-management-service";
+import { notificationServices } from "../services/notification-service";
 
 const taskManagementRoutes = express.Router();
 
 taskManagementRoutes.get("/:taskId/:userId", taskManagementServices.getOneTask);
-taskManagementRoutes.post("/", taskManagementServices.createTask);
+taskManagementRoutes.post("/", taskManagementServices.createTask, notificationServices.produceMessage);
 taskManagementRoutes.put("/:taskId", taskManagementServices.editTask);
 taskManagementRoutes.get("/user/:userId", taskManagementServices.getAllTask);
 taskManagementRoutes.delete("/:taskId/:userId", taskManagementServices.deleteTask);
--- main-api/src/services/task-management-service.ts
+++ main-api/src/services/task-management-service.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse, isAxiosError } from "axios";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const taskManagementURL = process.env.TASK_MANAGEMENT_SERVICE;
 
@@ -14,22 +14,29 @@ const getOneTask = (req: Request, res: Response) => {
       console.log(err);
       if (isAxiosError(err)) {
         console.log(err.response?.data);
-        return res.status(Number(err.response?.status)).send({ error: err.name, detail: err.response?.data });
+        return res
+          .status(isNaN(Number(err.response?.status)) ? 500 : Number(err.response?.status))
+          .send({ error: err.name, detail: err.response?.data });
       }
       res.status(500).send(err);
     });
 };
 
-const createTask = (req: Request, res: Response) => {
+const createTask = (req: Request, res: Response, next: NextFunction) => {
   const { userId, title, description, milestone, deadline } = req.body;
   axios
     .post(taskManagementURL + "/tasks", { userId, title, description, milestone, deadline })
-    .then((resp) => res.send(resp.data))
+    .then((resp) => {
+      req.body = { message: `user ${userId} membuat task baru dengan title ${title}, deadline: ${deadline}` };
+      next();
+    })
     .catch((err) => {
       console.log(err);
       if (isAxiosError(err)) {
         console.log(err.response?.data);
-        return res.status(Number(err.response?.status)).send({ error: err.name, detail: err.response?.data });
+        return res
+          .status(isNaN(Number(err.response?.status)) ? 500 : Number(err.response?.status))
+          .send({ error: err.name, detail: err.response?.data });
       }
       res.status(500).send(err);
     });
