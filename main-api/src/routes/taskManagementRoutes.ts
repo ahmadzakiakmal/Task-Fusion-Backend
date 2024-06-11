cbc4d95d9f73e0fb573774c6d90585600ebb77ca
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-06-11 14:45:46 +0700

    feat: map tm-service/get-task to main-api

--- main-api/src/routes/taskManagementRoutes.ts
+++ main-api/src/routes/taskManagementRoutes.ts
@@ -3,6 +3,7 @@ import { taskManagementServices } from "../services/task-management-service";
 
 const taskManagementRoutes = express.Router();
 
-taskManagementRoutes.post("/", taskManagementServices.createTask);
+taskManagementRoutes.get("/", taskManagementServices.createTask);
+taskManagementRoutes.post("/:taskId/:projectId", taskManagementServices.getOneTask);
 
 export default taskManagementRoutes;
--- main-api/src/services/task-management-service.ts
+++ main-api/src/services/task-management-service.ts
@@ -3,7 +3,22 @@ import { Request, Response } from "express";
 
 const taskManagementURL = process.env.TASK_MANAGEMENT_SERVICE;
 
-const getOneTask = (req: Request, res: Response) => {};
+const getOneTask = (req: Request, res: Response) => {
+  const { taskId, userId } = req.params;
+  axios
+    .post(taskManagementURL + `/tasks/${taskId}?userId=${userId}`)
+    .then((resp) => {
+      res.send(resp.data);
+    })
+    .catch((err) => {
+      console.log(err);
+      if (isAxiosError(err)) {
+        console.log(err.response?.data);
+        return res.status(Number(err.response?.status)).send({ error: err.name, detail: err.response?.data });
+      }
+      res.status(500).send(err);
+    });
+};
 
 const createTask = (req: Request, res: Response) => {
   const { userId, title, description, milestone, deadline } = req.body;
@@ -11,7 +26,7 @@ const createTask = (req: Request, res: Response) => {
     .post(taskManagementURL + "/tasks", { userId, title, description, milestone, deadline })
     .then((resp) => res.send(resp.data))
     .catch((err) => {
-      console.log(err)
+      console.log(err);
       if (isAxiosError(err)) {
         console.log(err.response?.data);
         return res.status(Number(err.response?.status)).send({ error: err.name, detail: err.response?.data });
